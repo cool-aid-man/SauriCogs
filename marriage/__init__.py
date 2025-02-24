5760836f1205ce33ad83154c6a7e4f1773d665a2
Cool aid <84676543+cool-aid-man@users.noreply.github.com>
2025-02-24 20:35:38 +0530

    lets see init

--- marriage/__init__.py
+++ marriage/__init__.py
@@ -1,22 +1,18 @@
 from asyncio import create_task
+
 from .marriage import Marriage
+
 
 async def setup_after_ready(bot):
     await bot.wait_until_red_ready()
     cog = Marriage(bot)
-    for command in cog.get_commands():
+    for name, command in cog.all_commands.items():
         if not command.parent:
-            # Prepend "m" to the command name if one already exists in the bot
-            if bot.get_command(command.name):
+            if bot.get_command(name):
                 command.name = f"m{command.name}"
-            # Update aliases similarly
-            new_aliases = []
             for alias in command.aliases:
                 if bot.get_command(alias):
-                    new_aliases.append(f"m{alias}")
-                else:
-                    new_aliases.append(alias)
-            command.aliases = new_aliases
+                    command.aliases[command.aliases.index(alias)] = f"m{alias}"
     await bot.add_cog(cog)
 
 async def setup(bot):
